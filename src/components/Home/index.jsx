83351379339fda5a738caf65cda7d5e96ba488bc
encepsuryana <encep.suryanajr@gmail.com>
2022-03-18 21:47:34 +0700

    Todo Method

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
-export default function index() {
-  return <div>Home Page</div>;
+export default function Home() {
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo('');
+  };
+
+  const handleChange = e => {
+    setTodo(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={todo} onChange={handleChange} />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
